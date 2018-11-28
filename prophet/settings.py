4b8ec1460bb2d3c25821be7914f0d09a12ed3a32
Olivia D'Angelo <olivia.dangelo@gmail.com>
2018-11-28 16:43:41 -0500

    fixing db, adding readme

--- prophet/settings.py
+++ prophet/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'prophet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prophet_db',
     }
 }
 
