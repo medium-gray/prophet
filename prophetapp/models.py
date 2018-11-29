ec76aea97f1b49d5f215c9c2377b1e226699b2a8
Olivia D'Angelo <olivia.dangelo@gmail.com>
2018-11-29 15:04:43 -0500

    adding allow null to bitcoin data model fields

--- prophetapp/models.py
+++ prophetapp/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class RawBitcoinData(models.Model):
     datetime = models.DateField(unique=True)
-    price_coinbase = models.FloatField()
-    price_bitstamp = models.FloatField()
-    volume_btc_coinbase = models.FloatField()
-    volume_btc_bitstamp = models.FloatField()
-    volume_currency_coinbase = models.FloatField()
-    volume_currency_bitstamp = models.FloatField()
-    wighted_price_coinbase = models.FloatField()
-    wighted_price_bitstamp = models.FloatField()
+    price_coinbase = models.FloatField(null=True)
+    price_bitstamp = models.FloatField(null=True)
+    volume_btc_coinbase = models.FloatField(null=True)
+    volume_btc_bitstamp = models.FloatField(null=True)
+    volume_currency_coinbase = models.FloatField(null=True)
+    volume_currency_bitstamp = models.FloatField(null=True)
+    wighted_price_coinbase = models.FloatField(null=True)
+    wighted_price_bitstamp = models.FloatField(null=True)
 
     def __str__(self):
         return self.price_coinbase
