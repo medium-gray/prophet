b7f36689f97842321818cc69625abf15a3bc74e1
Olivia D'Angelo <olivia.dangelo@gmail.com>
2018-11-29 15:57:45 -0500

    redoing migrations

--- prophetapp/models.py
+++ prophetapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class RawBitcoinData(models.Model):
-    datetime = models.DateField(primary_key=True)
+    datetime = models.DateTimeField(unique=True)
     price_coinbase = models.FloatField(null=True)
     price_bitstamp = models.FloatField(null=True)
     volume_btc_coinbase = models.FloatField(null=True)
