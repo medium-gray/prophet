8d53fc6fd685028058e46ee1ce65012cc2fbd534
Olivia D'Angelo <olivia.dangelo@gmail.com>
2018-11-29 14:29:47 -0500

    adding model and migration for bitcoin price data table

--- prophetapp/models.py
+++ prophetapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class RawBitcoinData(models.Model):
+    datetime = models.DateField()
+    price_coinbase = models.FloatField()
+    price_bitstamp = models.FloatField()
+    volume_btc_coinbase = models.FloatField()
+    volume_btc_bitstamp = models.FloatField()
+    volume_currency_coinbase = models.FloatField()
+    volume_currency_bitstamp = models.FloatField()
+    wighted_price_coinbase = models.FloatField()
+    wighted_price_bitstamp = models.FloatField()
+
+    def __str__(self):
+        return self.price_coinbase
